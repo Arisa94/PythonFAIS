3819b0474e91b6554c9a726d2b68a0c652aa5850
Arisa94 <arisa9404@gmail.com>
2016-11-30 02:59:16 +0100

    Update 4.5.py

--- 4/4.5.py
+++ 4/4.5.py
@@ -1,9 +1,20 @@
-def odwracanie(L, left, right):
-    L = L[:left] + L[left:(right + 1)][::-1] + L[(right + 1):]
-    return L
-   
-L = [1, 2, 3, 4, 10, 13, 9, 4, 5]
-left = 2
-right = 7
-result = odwracanie(L, left, right)
-print result
+def odwracanieIteracyjnie(L, left, right):
+	while left < right:
+		L[left], L[right] = L[right], L[left]
+		left += 1
+		right -= 1
+
+def odwracanieRekurencyjnie(L, left, right):
+	if(left < right):
+		L[left], L[right] = L[right], L[left]
+		odwracanieRekurencyjnie(L, left+1, right - 1)
+
+list  = [1, 2, 3, 4]
+print list
+odwracanieIteracyjnie(list, 0, 2);
+print list
+
+list  = [1, 2, 3, 4]
+print list
+odwracanieRekurencyjnie(list, 0, 2);
+print list
